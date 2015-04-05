4aea2b3b40770ef4decd8600f5e67574afcf5f96
Horven Tsai <htsai010@ucr.edu>
2015-04-05 16:23:21 -0700

    user input parsing, errors not fixed yet

--- src/uin.cpp
+++ src/uin.cpp
@@ -1,16 +1,63 @@
 #include <iostream>
+#include <vector>
 #include <string>
 
 using namespace std;
 
 void userin(string &);
+vector <string> parsed(const string);
 void print(const string);
+
 
 void userin(string &s)
 {
 	getline(cin,s);
 
 	return;
+}
+
+vector <string>parsed (const string s)
+{
+	vector <string>v;
+	string parsing = s;
+	string par = "";
+
+	int j = 0;
+	int k = 0;
+	for(int i = 0; i < parsing.size(); i++)
+	{
+		if(parsing.at(i) == ' ')
+		{
+			j = 0;
+			v.push_back(par);
+			v.at(k).append(parsing.at(i));
+			k++;
+			continue;
+		}
+		else if(parsing.at(i) != ' ')
+		{
+			if(j == 0)
+			{
+				v.push_back(par);
+				v.at(k).append(parsing.at(i));
+				j++;
+			}
+			else
+			{
+				v.at(k).append(parsing.at(i));
+				j++;
+			}
+		}
+	}
+
+	cout << "vector is as follows: ";
+	for(int i = 0; i < v.size(); i++)
+	{
+		cout << v.at(i);
+	}
+	cout << endl;
+
+	return v;
 }
 
 void print(const string s)
@@ -52,6 +99,7 @@ int main()
 	string uin;
 
 	userin(uin);
+	parsed(uin);
 	print(uin);
 
 	return 0;
