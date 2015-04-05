e340ab8abb12bf62fc6f707656c3a3e1cdddfc63
Horven Tsai <htsai010@ucr.edu>
2015-04-05 15:40:05 -0700

    added post confirmation

--- src/uin.cpp
+++ src/uin.cpp
@@ -9,13 +9,42 @@ void print(const string);
 void userin(string &s)
 {
 	getline(cin,s);
+
 	return;
 }
 
 void print(const string s)
 {
+	char check = ' ';
+	bool cont = false;
+
 	cout << "Your message is as follows:" << endl;
 	cout << s << endl;
+
+	cout << "Your message will tweet as follows:" << endl;
+	cout << s << "(" << s.size() << "/#" <<  endl;
+
+	while(!cont)
+	{
+		cout << "Do you wish to post? (Y or N then hit enter): ";
+		cin >> check;
+
+		if((check == 'Y') || (check == 'y') || (check == 'N') || (check == 'n'))
+		{
+			cont = true;
+		}
+		else
+		{
+			cout << "Please enter a valid choice. Y or y for yes, N or n for no." << endl;
+		}
+	}
+
+	if(check == 'Y' || check == 'y')
+	{
+		//post to twitter. push to twitter push library
+	}
+
+	return;
 }
 
 int main()
